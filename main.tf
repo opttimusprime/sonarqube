b01eeef32b539d5ae525459604551b874727a43d
devops_01 <devops_01@gmail.com>
2026-06-16 12:51:08 +0530

    HTTPS logic has been added....

--- main.tf
+++ main.tf
@@ -1,3 +1,37 @@
+resource "aws_security_group" "sonarqube_alb" {
+  name        = "${local.name}-alb-sg"
+  description = "Security group for SonarQube ALB"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    description = "HTTP from allowed CIDR"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_sonarqube_cidr]
+  }
+
+  ingress {
+    description = "HTTPS from allowed CIDR"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_sonarqube_cidr]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name}-alb-sg"
+  })
+}
+
 resource "aws_security_group" "sonarqube" {
   name        = "${local.name}-sg"
   description = "Security group for SonarQube"
@@ -12,11 +46,11 @@ resource "aws_security_group" "sonarqube" {
   }
 
   ingress {
-    description = "SonarQube UI"
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
-    cidr_blocks = [var.allowed_sonarqube_cidr]
+    description     = "SonarQube UI from ALB"
+    from_port       = 9000
+    to_port         = 9000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sonarqube_alb.id]
   }
 
   egress {
@@ -36,7 +70,7 @@ resource "aws_instance" "sonarqube" {
   ami                    = data.aws_ami.amazon_linux_2023.id
   instance_type          = var.instance_type
   key_name               = var.key_name
-  subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnets[0]
+  subnet_id              = data.terraform_remote_state.vpc.outputs.private_subnets[0]
   vpc_security_group_ids = [aws_security_group.sonarqube.id]
 
   user_data = file("${path.module}/userdata/sonarqube.sh")
@@ -51,19 +85,82 @@ resource "aws_instance" "sonarqube" {
   })
 }
 
-resource "aws_eip" "sonarqube" {
-  instance = aws_instance.sonarqube.id
-  domain   = "vpc"
+resource "aws_lb" "sonarqube" {
+  name               = "${local.name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sonarqube_alb.id]
+  subnets            = data.terraform_remote_state.vpc.outputs.public_subnets
 
   tags = merge(local.common_tags, {
-    Name = "${local.name}-eip"
+    Name = "${local.name}-alb"
   })
+}
+
+resource "aws_lb_target_group" "sonarqube" {
+  name     = "${local.name}-tg"
+  port     = 9000
+  protocol = "HTTP"
+  vpc_id   = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name}-tg"
+  })
+}
+
+resource "aws_lb_target_group_attachment" "sonarqube" {
+  target_group_arn = aws_lb_target_group.sonarqube.arn
+  target_id        = aws_instance.sonarqube.id
+  port             = 9000
+}
+
+resource "aws_lb_listener" "sonarqube_http" {
+  load_balancer_arn = aws_lb.sonarqube.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "sonarqube_https" {
+  load_balancer_arn = aws_lb.sonarqube.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = "arn:aws:acm:us-east-1:942548380129:certificate/04035cfa-147e-46be-aca6-93361d8a3c0b"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.sonarqube.arn
+  }
 }
 
 resource "aws_route53_record" "sonarqube" {
   zone_id = data.aws_route53_zone.main.zone_id
   name    = "sonarqube.${var.domain_name}"
   type    = "A"
-  ttl     = 300
-  records = [aws_eip.sonarqube.public_ip]
+
+  alias {
+    name                   = aws_lb.sonarqube.dns_name
+    zone_id                = aws_lb.sonarqube.zone_id
+    evaluate_target_health = true
+  }
 }
