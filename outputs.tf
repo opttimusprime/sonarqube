ed4b273f0c400cf7a2fc26df1cf1e1f623f2ca0b
devops_01 <devops_01@gmail.com>
2026-06-20 13:19:10 +0530

    EIP concept has been replaced....

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
-output "sonarqube_public_ip" {
-  value = aws_eip.sonarqube.public_ip
+output "sonarqube_alb_dns_name" {
+  value = aws_lb.sonarqube.dns_name
 }
 
 output "sonarqube_private_ip" {
@@ -7,5 +7,5 @@ output "sonarqube_private_ip" {
 }
 
 output "sonarqube_url" {
-  value = "http://sonarqube.${var.domain_name}:9000"
+  value = "https://sonarqube.${var.domain_name}"
 }
